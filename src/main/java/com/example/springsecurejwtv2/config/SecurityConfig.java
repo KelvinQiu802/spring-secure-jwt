bfb9f71774f19a39005cd1f4ff0e7cf4429e6f4a
KelvinQiu <kelvinqiu802@outlook.com>
2024-03-05 12:52:29 +0800

    feat: add jwt auth filter

--- src/main/java/com/example/springsecurejwtv2/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurejwtv2/config/SecurityConfig.java
@@ -1,20 +1,28 @@
 package com.example.springsecurejwtv2.config;
 
+import com.example.springsecurejwtv2.auth.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
-                req -> req.anyRequest().authenticated()
-        );
+        http
+                .authorizeHttpRequests(
+                        req -> req.anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
