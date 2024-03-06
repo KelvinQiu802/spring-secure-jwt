91d4c8fadd2fda39fed2eda2f437009a0aa009b3
KelvinQiu <kelvinqiu802@outlook.com>
2024-03-06 18:16:56 +0800

    feat: add filter to the config

--- src/main/java/com/example/springsecurejwtv2/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurejwtv2/config/SecurityConfig.java
@@ -1,13 +1,23 @@
 package com.example.springsecurejwtv2.config;
 
 import com.example.springsecurejwtv2.auth.JwtAuthFilter;
+import com.example.springsecurejwtv2.services.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -15,6 +25,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
+    private final CustomUserDetailsService customUserDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -22,8 +33,20 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         req -> req.anyRequest().authenticated()
                 )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        List<AuthenticationProvider> providers = new ArrayList<>();
+        DaoAuthenticationProvider daoAuthenticationProvider =
+                new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());  // TODO: use real encoder
+        daoAuthenticationProvider.setUserDetailsService(customUserDetailsService);
+        providers.add(daoAuthenticationProvider);
+        return new ProviderManager(providers);
     }
 
 }
