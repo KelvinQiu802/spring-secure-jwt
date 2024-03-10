43ba304ae481be8f1691c8e2248b0494cb679360
KelvinQiu <kelvinqiu802@outlook.com>
2024-03-10 21:31:11 +0800

    feat: show userName

--- src/main/java/com/example/springsecurejwtv2/controller/HomeController.java
+++ src/main/java/com/example/springsecurejwtv2/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.springsecurejwtv2.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +12,8 @@ public class HomeController {
 
     @GetMapping("/home")
     public String home() {
-        return "Hello World";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "Hello " + authentication.getName();
     }
 
 }
