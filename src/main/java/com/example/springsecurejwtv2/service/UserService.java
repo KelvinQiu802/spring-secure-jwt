a5e70c226707c4185c2aa6594cd1b9b10cb997cd
KelvinQiu <kelvinqiu802@outlook.com>
2024-03-11 08:39:52 +0800

    feat: get all users

--- src/main/java/com/example/springsecurejwtv2/service/UserService.java
+++ src/main/java/com/example/springsecurejwtv2/service/UserService.java
@@ -2,12 +2,15 @@ package com.example.springsecurejwtv2.service;
 
 import com.example.springsecurejwtv2.exception.UserNameAlreadyExistsException;
 import com.example.springsecurejwtv2.model.RegisterRequest;
+import com.example.springsecurejwtv2.model.UserDTO;
 import com.example.springsecurejwtv2.model.UserEntity;
 import com.example.springsecurejwtv2.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,6 +18,7 @@ import java.util.Optional;
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final ModelMapper modelMapper;
 
     public void createNewUser(RegisterRequest registerRequest) throws UserNameAlreadyExistsException {
         Optional<UserEntity> result = userRepository.findById(registerRequest.getUserName());
@@ -30,6 +34,11 @@ public class UserService {
                         .email(registerRequest.getEmail())
                         .build())
         );
+    }
+
+    public List<UserDTO> getAllUsers() {
+        List<UserEntity> userEntities = userRepository.findAll();
+        return userEntities.stream().map(userEntity -> modelMapper.map(userEntity, UserDTO.class)).toList();
     }
 
 }
